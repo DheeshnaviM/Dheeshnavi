fccdfea6c01e906985bbd186bbc3036fbf30f124
DheeshnaviM <Dheeshnavi@gmail.com>
2025-04-27 14:46:39 +0530

    Add files via upload

---
+++ Week1/day1/package-info.java
@@ -0,0 +1,17 @@
+package Week1.day1;
+
+package class chrome{
+	public static void main(string[] args)
+	
+			{ float version=91.0f;
+			System.out.print1n(version);
+			String developer="google";
+			System.out.print1n(developer);
+			Boolean Beta= false;
+			System.out.print1n(Beta);
+			int releaseyear=2008;
+			System.out.print1n(releaseyear);
+			char shortcutkeys='c';
+			System.out.println(shortcutkey);
+	}
+	}
